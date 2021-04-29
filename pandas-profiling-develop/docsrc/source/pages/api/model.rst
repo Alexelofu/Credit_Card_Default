3ffa72e2ca48fe60d30e4bfb53eeef22d0951302
Alexelofu <ofuonyeadialexander1997@gmail.com>
2021-04-29 14:12:08 +0100

    API update

---
+++ pandas-profiling-develop/docsrc/source/pages/api/model.rst
@@ -0,0 +1,24 @@
+=====
+Model
+=====
+
+.. currentmodule:: pandas_profiling.model
+.. toctree::
+
+.. autosummary::
+   :toctree: _autosummary
+
+   describe
+   handler
+   summary
+   summary_algorithms
+   summary_helpers
+   summary_helpers_image
+   summarizer
+   typeset
+   typeset_relations
+   messages
+   correlations
+   duplicates
+   sample
+   expectation_algorithms
--- project/app.py
+++ project/app.py
@@ -2,49 +2,26 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
+app = Flask(__name__)
+
 filename = 'model/new_model'
 loaded_model = pickle.load(open(filename, 'rb'))
 
-
-app = Flask(__name__)
-
-
+col=['LIMIT','SEX','EDUCATION','MARRIAGE','AGE','PAY_0','PAY_2','PAY_3','PAY_4','PAY_5','PAY_6','BILL_AMT1','BILL_AMT2','BILL_AMT3', 'BILL_AMT4','BILL_AMT5','BILL_AMT6','PAY_AMT1','PAY_AMT2','PAY_AMT3', 'PAY_AMT4','PAY_AMT5','PAY_AMT6' ]
 
 @app.route('/')
-def man():
-    return render_template('home.html')
+def hello_world():
+    return render_template("index.html")
 
 
-@app.route('/predict', methods=['POST'])
-def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    data5 = request.form['e']
-    data6 = request.form['f']
-    data7 = request.form['g']
-    data8 = request.form['h']
-    data9 = request.form['i']
-    data10 = request.form['j']
-    data11 = request.form['k']
-    data12 = request.form['l']
-    data13 = request.form['m']
-    data14 = request.form['n']
-    data15 = request.form['o']
-    data16 = request.form['p']
-    data17 = request.form['q']
-    data18 = request.form['r']
-    data19 = request.form['s']
-    data20 = request.form['t']
-    data21 = request.form['u']
-    data22 = request.form['v']
-    data23 = request.form['w']
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final=[np.array(int_features, dtype=float)]
+    prob=loaded_model.predict(final)
     
-    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23]])
-    prob = loaded_model.predict(arr)
+
     return render_template('after.html', data=prob)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
